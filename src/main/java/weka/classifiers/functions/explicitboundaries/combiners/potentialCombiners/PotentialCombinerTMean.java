b9a897c6af85c983934b4e145001cbc451367946
PT <pawel.trajdos@pwr.edu.pl>
2019-01-23 09:48:51 +0100

    1. Global TMeanCombiner.

--- src/main/java/weka/classifiers/functions/explicitboundaries/combiners/potentialCombiners/PotentialCombinerTMean.java
+++ src/main/java/weka/classifiers/functions/explicitboundaries/combiners/potentialCombiners/PotentialCombinerTMean.java
@@ -4,7 +4,6 @@
 package weka.classifiers.functions.explicitboundaries.combiners.potentialCombiners;
 
 import java.io.Serializable;
-import java.util.LinkedList;
 import java.util.List;
 
 import weka.classifiers.functions.explicitboundaries.DecisionBoundaries;
@@ -15,11 +14,11 @@ import weka.core.UtilsPT;
 
 /**
  * @author Pawel Trajdos
- * @version 1.2.0
- * Combines the boundaries-specific outputs using max-max rule
+ * @version 1.4.0
+ * Combines the boundaries-specific outputs using Truncated mean rule
  *
  */
-public class PotentialCombinerMaxTMean implements PotentialCombiner, Serializable {
+public class PotentialCombinerTMean implements PotentialCombiner, Serializable {
 
 	/**
 	 * 
@@ -40,43 +39,16 @@ public class PotentialCombinerMaxTMean implements PotentialCombiner, Serializabl
 		int idx1 = boundariesList.get(0).getClass1Idx();
 		int idx2 = boundariesList.get(0).getClass2Idx();
 		
-		LinkedList<Double> negatives = new LinkedList<Double>();
-		LinkedList<Double> positives = new LinkedList<Double>();
-		double tmpVal;
+	
+		double[] tmpVals = new double[boundNumber];
 		
 		for(int i=0;i<boundNumber;i++) {
-			tmpVal = potFunction.getPotentialValue( boundariesList.get(i).getValue(inst));
-			if(tmpVal <0)
-				negatives.add(tmpVal);
-			
-			if(tmpVal>0)
-				positives.add(tmpVal);
-			
+			tmpVals[i] = potFunction.getPotentialValue( boundariesList.get(i).getValue(inst));	
 		}
-		if(negatives.size() == 0)
-			return idx1;
-		
-		if(positives.size() == 0)
-			return idx2;
-		
-		int negS = negatives.size();
-		double[] negativeArray = new double[negS];
-		for(int i=0;i<negS;i++) {
-			negativeArray[i]=negatives.get(i);
-		}
-		
-		int posS  = positives.size();
-		double[] positiveArray = new double[posS];
-		for(int i=0;i<posS;i++) {
-			positiveArray[i] = positives.get(i);
-		}
-		
-		double negTMean  = UtilsPT.truncatedMean(negativeArray);
-		double posTMean = UtilsPT.truncatedMean(positiveArray);
-		
+		double result = UtilsPT.truncatedMean(tmpVals);
 	
 	
-		return (posTMean - negTMean)>0? idx1:idx2;
+		return (result)>0? idx1:idx2;
 	}
 
 }
